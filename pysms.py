51dc0cfe2f78803df0a56367cda4dd464fdb9775
notkilometers <47023811+notkilometers@users.noreply.github.com>
2020-06-09 00:23:02 -0700

    added carrier lookup and support for other email

--- pysms.py
+++ pysms.py
@@ -1,27 +1,66 @@
-import smtplib, ssl
+import smtplib, ssl, json, requests
 # doesnt sanatize input but thats not my problem
-#only uses gmail and at&t for the first iteration, will add other options later
-#adding carrier lookup with numverify api 
-class Number:
-    def __init__(self, selfUser, selfPass , targetN, targetM, targetC):
-        self.selfUser = selfUser 
+#only supports ATT due to not having enough numbers to compare to API output
+# adding carrier lookup with numverify api 
+class SMS:
+    def __init__(self, selfUser, selfPass , targetN, targetNF):
+        self.selfUser = selfUser
         self.selfPass = selfPass
-        self.targetN = targetN + "@txt.att.net"
-        self.targetM = targetM
-        self.targetC = targetC
-        self.port = 465 # for ssl
-        self.smtp = "smtp.gmail.com"
-        self.context =  ssl.create_default_context()    
+        self.targetN = targetN
+        self.context =  ssl.create_default_context()
+        api = "" # put api key here, get from numverify.com for 250 free lookups
+        url = "http://apilayer.net/api/validate?access_key=" + api +"&number=" + "1" + self.targetN
+        req = requests.get(url)
+        resp = repr(req.json())
+        print(resp)
+        linetype = resp.split("line_type': '")[1]
+        print(linetype)
+        linetype = linetype.split("'")[0]
+        if (linetype == "landline"):
+            print("Number type not supported (landline)")
+        else:
+            print("not landline, continuing")
+        carrier = resp.split("carrier': '")[1]
+        carrier = carrier.split("'")[0]
+        print(carrier)
+        if (carrier == "AT&T Mobility LLC"):
+            cAdd = "@txt.att.net"
+        else:
+            cAdd = "@txt.att.net" #default to using most common carrier
+        smtpString = self.selfUser.split("@")[1]
+        if (smtpString == "gmail.com"):
+            self.smtp = "smtp.gmail.com"
+            self.port = 465 #ssl
+        elif(smtpString == "outlook.com"):
+            self.smtp = "smtp-mail.outlook.com"
+            self.port = 587 #tls
+        elif (smtpString == "yahoo.com"):
+            self.smtp = "smtp.mail.yahoo.com"
+            self.port = 465 #ssl
+        elif (smtpString == "icloud.com"):
+            self.smtp = "smtp.mail.me.com"
+            self.port = 587 #tls
+        elif (smtpString == "aol.com"):
+            self.smtp = "smtp.aol.com"
+            self.port = 465 #ssl
+        elif (smtpString == "mail.com"):
+            self.smtp = "smtp.mail.com"
+            self.port = 587 #tls
+        else:
+            print("Email host not supported yet, existing.")
+            exit()
+        targetNF = targetN + cAdd
+        self.targetNF = targetNF
     @classmethod
     def getdata(cls):
         return cls(
             selfUser = input("Enter Your Email Including @ : "),
             selfPass = input("Enter Your Password : "),
             targetN = input("Enter Target Phone Number : "),
-            targetC = input("Enter Target Phone Carrier : "),
-            targetM = input("Enter Desired Message : ")
+            targetNF = ""
         ) 
     def send(self):
         with smtplib.SMTP_SSL(self.smtp, self.port, context=self.context) as server:
+            targetM = input("Enter Desired Message : ")
             server.login(self.selfUser, self.selfPass)
-            server.sendmail(self.selfUser, self.targetN, self.targetM)
+            server.sendmail(self.selfUser, self.targetNF, targetM)
