0243f04efeb44dfb027c850525ac0e65739388f7
notkilometers <47023811+notkilometers@users.noreply.github.com>
2020-06-14 23:22:05 -0700

    added email module, fixed structure

--- pysms.py
+++ pysms.py
@@ -23,13 +23,37 @@ def cLookup(targetN, carrier=""):
     print(carrier)
     return carrier
 
+def sendSetup(self, selfUser):
+    self.context =  ssl.create_default_context()
+    smtpString = self.selfUser.split("@")[1]
+    if (smtpString == "gmail.com"):
+        self.smtp = "smtp.gmail.com"
+        self.port = 465 # ssl
+    elif(smtpString == "outlook.com"):
+        self.smtp = "smtp-mail.outlook.com"
+        self.port = 587 # tls
+    elif (smtpString == "yahoo.com"):
+        self.smtp = "smtp.mail.yahoo.com"
+        self.port = 465 # ssl
+    elif (smtpString == "icloud.com"):
+        self.smtp = "smtp.mail.me.com"
+        self.port = 587 # tls
+    elif (smtpString == "aol.com"):
+        self.smtp = "smtp.aol.com"
+        self.port = 465 # ssl
+    elif (smtpString == "mail.com"):
+        self.smtp = "smtp.mail.com"
+        self.port = 587 #tls
+    else:
+        print("Email host not supported yet, existing.")
+        exit()
+            
 class SMS:
     def __init__(self, selfUser, selfPass , targetN, lookupYN="", cAdd="", targetNF="", carrier="", fillin=0):
         targetN = str(targetN)
         self.selfUser = selfUser
         self.selfPass = selfPass
         self.targetN = targetN
-        self.context =  ssl.create_default_context()
         if (lookupYN == "true" or lookupYN == "True" or lookupYN == "yes" or lookupYN == "Yes" or lookupYN == "y" or lookupYN == "Y"):
             cLookup(self.targetN)
         #elif():
@@ -40,30 +64,9 @@ class SMS:
             pass
         else:
             cAdd = "@txt.att.net" # default to using most common carrier
-        smtpString = self.selfUser.split("@")[1]
-        if (smtpString == "gmail.com"):
-            self.smtp = "smtp.gmail.com"
-            self.port = 465 # ssl
-        elif(smtpString == "outlook.com"):
-            self.smtp = "smtp-mail.outlook.com"
-            self.port = 587 # tls
-        elif (smtpString == "yahoo.com"):
-            self.smtp = "smtp.mail.yahoo.com"
-            self.port = 465 # ssl
-        elif (smtpString == "icloud.com"):
-            self.smtp = "smtp.mail.me.com"
-            self.port = 587 # tls
-        elif (smtpString == "aol.com"):
-            self.smtp = "smtp.aol.com"
-            self.port = 465 # ssl
-        elif (smtpString == "mail.com"):
-            self.smtp = "smtp.mail.com"
-            self.port = 587 #tls
-        else:
-            print("Email host not supported yet, existing.")
-            exit()
         targetNF = targetN + cAdd
         self.targetNF = targetNF
+        sendSetup(self, self.selfUser)
     @classmethod
     def getdata(cls):
         return cls(
@@ -72,8 +75,7 @@ class SMS:
             selfPass = input("Enter Your Password : "),
             targetN = input("Enter Target Phone Number : "),
             cAdd = input("(leave blank if unknown)\nEnter SMS string for carrier including @ : "),
-            targetNF = "",
-            fillin = 1
+            targetNF = ""
         ) 
     def send(self, targetM):
         with smtplib.SMTP_SSL(self.smtp, self.port, context=self.context) as server:
@@ -81,3 +83,28 @@ class SMS:
                 targetM = input("Enter Desired Message : ")
             server.login(self.selfUser, self.selfPass)
             server.sendmail(self.selfUser, self.targetNF, targetM)
+
+class Email:
+    def __init__(self, selfUser,  selfPass, targetEmail):
+        self.selfUser = selfUser
+        self.selfPass = selfPass
+        self.targetEmail = targetEmail
+        sendSetup(self, self.selfUser)
+        
+
+    def send(self, targetM):
+        with smtplib.SMTP_SSL(self.smtp, self.port, context=self.context) as server:
+            if(targetM == ""):
+                targetM = input("Enter Desired Message : ")
+            server.login(self.selfUser, self.selfPass)
+            server.sendmail(self.selfUser, self.targetEmail, targetM)
+    @classmethod
+    def getdata(cls):
+        return cls(
+            lookupYN = input("Look Up Carrier for Phone Number? "),
+            selfUser = input("Enter Your Email Including @ : "),
+            selfPass = input("Enter Your Password : "),
+            targetN = input("Enter Target Phone Number : "),
+            cAdd = input("(leave blank if unknown)\nEnter SMS string for carrier including @ : "),
+            targetNF = ""
+        ) 
